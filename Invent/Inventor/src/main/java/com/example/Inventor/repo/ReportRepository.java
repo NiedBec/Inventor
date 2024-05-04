d1effbb0dfd4c4918e68f302f5ebdf8cb12cdfc0
NiedBec <103964942+NiedBec@users.noreply.github.com>
2024-05-04 16:05:50 +0300

    Delete Invent directory

--- Invent/Inventor/src/main/java/com/example/Inventor/repo/ReportRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Inventor.repo;
-
-import com.example.Inventor.models.Report;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ReportRepository extends CrudRepository<Report,Long> {
-}
